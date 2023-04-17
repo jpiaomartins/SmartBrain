3415525f290a3d3b09ef73206ee98d481a73667a
João Martins <joao.martins.ext@spms.min-saude.pt>
2023-04-17 09:26:07 +0100

    FaceRecognition Component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,78 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className='App'>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-      <ParticlesBg color="#ffffff" num={200} type="cobweb" bg={true} />
-    </div>
-  );
+// Setting up Clarify
+const clarifyReturnRequestOptions = (imageURL) => {
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": 'a56gt81kb0',
+        "app_id": 'smartbrain'
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": imageURL
+                }
+            }
+        }
+    ]
+  });
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + '3f3bbbfb451d4d91bf516dd6088599a4'
+    },
+    body: raw
+  };
+
+  return requestOptions;
+};
+
+// App Component Definition
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    let MODEL_ID = 'face-detection';
+    let REQUEST_OPTIONS = clarifyReturnRequestOptions(this.state.input);
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", REQUEST_OPTIONS)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+  }
+
+
+  render() {
+    let {input, imageUrl} = this.state;
+    return (
+      <div className='App'>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={imageUrl}/>
+        <ParticlesBg color="#ffffff" num={200} type="cobweb" bg={true} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className="f3">{'This Magic Brain will detect faces in your pictures!'}</p>
             <div className="center">
                 <div className="center form pa4 br3 shadow-5">
-                    <input className = "f4 pa2 w-70 center" type='text' placeholder="URL Link"/>
-                    <button className = "w-30 grow link ph3 pv2 dib white bg-light-blue" type="button">Detect</button>
+                    <input className = "f4 pa2 w-70 center" type='text' placeholder="Image URL" onChange={onInputChange}/>
+                    <button className = "w-30 grow link ph3 pv2 dib white bg-light-blue" type="button" onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
         </div>
